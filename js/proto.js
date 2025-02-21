b020d8926d2935d50b9b4f2752290fc3f10759b8
linfanghong <linfanghong1997@Gmail.com>
2025-02-21 10:55:43 +0800

    feat: 继承

---
+++ js/proto.js
@@ -0,0 +1,126 @@
+// 原型链继承
+
+// function Father() {
+//   this.colors = ['red', 'green', 'yellow']
+// }
+
+// function Son() {}
+
+// Son.prototype = new Father()
+
+// const son1 = new Son()
+
+// const son2 = new Son()
+
+// son1.colors.push('blue')
+
+// console.log(son2.colors)
+
+// 经典继承
+
+// function Father() {
+//   this.colors = ['red', 'green', 'yellow']
+// }
+
+// function Son() {
+//   Father.call(this)
+// }
+
+// const son1 = new Son()
+
+// son1.colors.push('blue')
+
+// const son2 = new Son()
+
+// console.log(son1.colors, son2.colors)
+
+// 组合继承
+
+// function Father() {
+//   this.colors = ['red', 'green', 'yellow']
+// }
+
+// Father.prototype.addColor = function (newColor) {
+//   this.colors.push(newColor)
+// }
+
+// function Son() {
+//   Father.call(this)
+// }
+
+// Son.prototype = new Father()
+
+// const son1 = new Son()
+
+// son1.addColor('blue')
+
+// const son2 = new Son()
+
+// console.log(son1.colors, son2.colors)
+
+// 原型继承
+
+// function Father() {
+//   this.colors = ['red', 'green', 'yellow']
+// }
+
+// Father.prototype.addColor = function (newColor) {
+//   this.colors.push(newColor)
+// }
+
+// const father = new Father()
+
+// const son1 = Object.create(father)
+
+// const son2 = Object.create(father)
+
+// 寄生继承
+
+// function Father() {
+//   this.colors = ['red', 'green', 'yellow']
+// }
+
+// Father.prototype.getColor = function () {
+//   return this.colors
+// }
+
+// const father = new Father()
+
+// function createAnther(obj) {
+//   const ins = Object.create(obj)
+//   ins.addColor = function (newColor) {
+//     this.colors.push(newColor)
+//   }
+//   return ins
+// }
+
+// const son1 = createAnther(father)
+
+// const son2 = createAnther(father)
+
+// 寄生组合继承
+
+// function inherit(Father, Son) {
+//   Son.prototype = Object.create(Father.prototype)
+//   Son.prototype.constructor = Son
+// }
+
+// function Father() {
+//   this.colors = ['red', 'green', 'yellow']
+// }
+
+// Father.prototype.getColor = function () {
+//   return this.colors
+// }
+
+// function Son() {
+//   Father.call(this)
+// }
+
+// inherit(Father, Son)
+
+// const son1 = new Son()
+
+// const son2 = new Son()
+
+// ES6 class
