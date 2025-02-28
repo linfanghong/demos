4db4c20192c376ec5d36325087db6bccf097f189
linfanghong <linfanghong1997@Gmail.com>
2025-02-28 11:02:01 +0800

    feat: Promise实现

--- js/promise.js
+++ js/promise.js
@@ -1,110 +1,109 @@
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     reject('Error')
-//   }, 1000)
+// const p1 = new Promise((resolve, reject) => {
+//   console.log('create a promise')
+//   resolve('成功了')
 // })
 
-// promise
-//   .then((res) => {
-//     console.log('111', res)
-//   })
-//   .catch((err) => {
-//     console.log('222', err)
-//   })
+// console.log('after new promise')
+
+// const p2 = p1.then((data) => {
+//   console.log(data)
+//   throw new Error('失败了')
+// })
+
+// const p3 = p2.then(
+//   (data) => {
+//     console.log('success', data)
+//   },
+//   (err) => {
+//     console.log('failed', err)
+//   }
+// )
 
 class SelfPromise {
   constructor(executor) {
     this.state = 'pending'
+    this.value = null
+    this.reason = null
     this.resolveHandlerList = []
     this.rejectHandlerList = []
-    executor(this.resolve, this.reject)
+    try {
+      executor(this.resolve, this.reject)
+    } catch (err) {
+      this.reject(err)
+    }
   }
-  resolve = (result) => {
+  resolve = (value) => {
     if (this.state === 'pending') {
       this.state = 'fulfilled'
-      const fn = this.resolveHandlerList.shift()
-      return fn(result)
+      this.value = value
+      const resolveFunc = this.resolveHandlerList.shift()
+      resolveFunc && resolveFunc(this.value)
     }
   }
-  reject = (error) => {
+  reject = (reason) => {
     if (this.state === 'pending') {
       this.state = 'rejected'
+      this.reason = reason
+      const rejectFunc = this.rejectHandlerList.shift()
+      rejectFunc && rejectFunc(this.reason)
     }
   }
-  then = (...args) => {
-    this.resolveHandlerList.push(...args)
-    return this
+  then = (onFulfilled, onRejected) => {
+    if (!onFulfilled) {
+      return this
+    }
+    return new SelfPromise((resolve, reject) => {
+      try {
+        if (this.state === 'pending') {
+          this.resolveHandlerList.push(onFulfilled)
+          this.rejectHandlerList.push(onRejected)
+        }
+        if (this.state === 'fulfilled' && this.value && onFulfilled) {
+          resolve(onFulfilled(this.value))
+        }
+        if (this.state === 'rejected' && this.reason && onRejected) {
+          reject(onRejected(this.reason))
+        }
+      } catch (err) {
+        reject(err)
+      }
+    })
   }
   catch = (onRejected) => {
-    this.resolveHandlerList.push(onRejected)
-    return this
+    return new SelfPromise((resolve, reject) => {
+      try {
+        if (this.state === 'pending') {
+          this.rejectHandlerList.push(onRejected)
+        }
+        if (this.state === 'rejected' && this.reason && onRejected) {
+          reject(onRejected(this.reason))
+        }
+      } catch (err) {
+        reject(err)
+      }
+    })
   }
 }
 
-SelfPromise.all = (promises) => {
-  return new SelfPromise((resolve, reject) => {
-    const res = []
-    try {
-      promises.forEach((promise) => {
-        promise
-          .then((data) => {
-            res.push(data)
-            if (res.length === promises.length) {
-              resolve(res)
-            }
-          })
-          .catch((err) => {
-            reject(err)
-          })
-      })
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
-
-SelfPromise.race = (promises) => {
-  return new SelfPromise((resolve, reject) => {
-    promises.forEach((promise) => {
-      promise.then(resolve).catch(reject)
-    })
-  })
-}
-
-const promise = new SelfPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('result')
-  }, 1000)
+const n1 = new SelfPromise((resolve, reject) => {
+  console.log('create a promise')
+  resolve('成功了')
 })
 
-promise.then((res) => {
-  console.log('111', res)
+console.log('after new promise')
+
+const n2 = n1.then((data) => {
+  console.log(data)
+  throw new Error('失败了')
 })
 
-const promise1 = new SelfPromise((resolve, reject) => {
-  setTimeout(() => {
-    reject('result1')
-  }, 5e2)
-})
+const n3 = n2.then()
 
-const promise2 = new SelfPromise((resolve) => {
-  setTimeout(() => {
-    resolve('result2')
-  }, 1e3)
-})
-
-SelfPromise.race([promise1, promise2])
-  .then((res) => {
-    console.log('race resolve', res)
-  })
-  .catch((err) => {
-    console.log('race reject', err)
-  })
-
-SelfPromise.all([promise1, promise2])
-  .then((res) => {
-    console.log('all resolve', res)
-  })
-  .catch((err) => {
-    console.log('all reject', err)
-  })
+n3.then(
+  (data) => {
+    console.log('success', data)
+  },
+  (err) => {
+    console.log('failed', err)
+  }
+)
