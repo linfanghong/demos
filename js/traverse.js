4470a810137e9d9b75affee1e8a9f7f9ee048483
linfanghong <linfanghong1997@Gmail.com>
2025-03-18 15:45:19 +0800

    fix: 遍历

--- js/traverse.js
+++ js/traverse.js
@@ -15,21 +15,26 @@ person.name = 'John'
 
 person.age = 20
 
-// 第一种 for in 遍历的是可继承属性可遍历属性
+person[Symbol('id')] = '001'
+
+// 第一种 for in 遍历的是可继承属性可枚举属性
 for (attr in person) {
   console.log('111', attr, person[attr])
 }
 
-// 使用Object的静态方法
+// 使用Object的静态方法 可枚举属性
 console.log(
-  '111',
+  '222',
   Object.keys(person),
   Object.values(person),
   Object.entries(person)
 )
 
-console.log('111', Object.getOwnPropertyNames(person))
+// 使用Object的静态方法 可枚举属性 + 不可枚举属性
+console.log('333', Object.getOwnPropertyNames(person))
 
-console.log('111', Object.getOwnPropertySymbols(person))
+// 使用Object的静态方法 可枚举属性 + 不可枚举属性 + Symbol属性
+console.log('444', Object.getOwnPropertySymbols(person))
 
-console.log('111', Reflect.ownKeys(person))
+// 使用Reflect的静态方法 可枚举属性 + 不可枚举属性 + Symbol属性
+console.log('555', Reflect.ownKeys(person))
