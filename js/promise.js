75199b8464c6ca602705d802d4b88837a64e5fe9
linfanghong <linfanghong1997@Gmail.com>
2025-03-19 17:31:35 +0800

    feat: 完整版promise

--- js/promise.js
+++ js/promise.js
@@ -1,79 +1,174 @@
-class SelfPromise {
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
+class MyPromise {
   constructor(executor) {
-    this.state = 'pending'
-    this.value = null
-    this.reason = null
-    this.resolveHandlerList = []
-    this.rejectHandlerList = []
     try {
       executor(this.resolve, this.reject)
     } catch (err) {
       this.reject(err)
     }
   }
+  status = PENDING
+  value = null
+  reason = null
+  onFulfilledCallbacks = []
+  onRejectedCallbacks = []
+  static resolve = (param) => {
+    if (param instanceof MyPromise) {
+      return param
+    } else {
+      return new MyPromise((resolve) => resolve(param))
+    }
+  }
+  static reject = (reason) => {
+    return new MyPromise((resolve, reject) => reject(reason))
+  }
   resolve = (value) => {
-    if (this.state === 'pending') {
-      this.state = 'fulfilled'
+    if (this.status === PENDING) {
+      this.status = FULFILLED
       this.value = value
-      this.resolveHandlerList.forEach((fn) => {
-        fn(value)
-      })
+      while (this.onFulfilledCallbacks.length) {
+        this.onFulfilledCallbacks.shift()(value)
+      }
     }
   }
   reject = (reason) => {
-    if (this.state === 'pending') {
-      this.state = 'rejected'
+    if (this.status === PENDING) {
+      this.status = REJECTED
       this.reason = reason
-      this.rejectHandlerList.forEach((fn) => {
-        fn(reason)
-      })
+      while (this.onRejectedCallbacks.length) {
+        this.onRejectedCallbacks.shift()(reason)
+      }
     }
   }
   then = (onFulfilled, onRejected) => {
-    const promise2 = new SelfPromise((resolve, reject) => {
-      if (this.state === 'fulfilled') {
-        const result = onFulfilled(this.value)
+    onFulfilled =
+      typeof onFulfilled === 'function' ? onFulfilled : (value) => value
+    onRejected =
+      typeof onRejected === 'function'
+        ? onRejected
+        : (reason) => {
+            throw reason
+          }
+    const newPromise = new MyPromise((resolve, reject) => {
+      if (this.status === FULFILLED) {
         queueMicrotask(() => {
-          if (promise2 === result) {
-            return reject(
-              new TypeError('Chaining cycle detected for promise #<Promise>')
-            )
-          } else if (result instanceof SelfPromise) {
-            result.then(resolve, reject)
-          } else {
-            resolve(result)
+          try {
+            const result = onFulfilled(this.value)
+            resolvePromise(result, newPromise, resolve, reject)
+          } catch (err) {
+            reject(err)
           }
         })
-      } else if (this.state === 'rejected') {
-        const result = onRejected(this.reason)
-        reject(result)
-      } else if (this.state === 'pending') {
-        this.resolveHandlerList.push(() => {
-          setTimeout(onFulfilled, 0)
+      } else if (this.status === REJECTED) {
+        queueMicrotask(() => {
+          try {
+            const result = onRejected(this.reason)
+            resolvePromise(result, newPromise, resolve, reject)
+          } catch (err) {
+            reject(err)
+          }
         })
-        this.rejectHandlerList.push(() => {
-          setTimeout(onRejected, 0)
+      } else if (this.status === PENDING) {
+        this.onFulfilledCallbacks.push(() => {
+          queueMicrotask(() => {
+            try {
+              const result = onFulfilled(this.value)
+              resolvePromise(result, newPromise, resolve, reject)
+            } catch (err) {
+              reject(err)
+            }
+          })
+        })
+        this.onRejectedCallbacks.push(() => {
+          queueMicrotask(() => {
+            try {
+              const result = onRejected(this.reason)
+              resolvePromise(result, newPromise, resolve, reject)
+            } catch (err) {
+              reject(err)
+            }
+          })
         })
       }
     })
-
-    return promise2
+    return newPromise
   }
 }
 
-const n1 = new SelfPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('成功了')
-  }, 1000)
-  console.log('create a promise')
-})
+function resolvePromise(promise, x, resolve, reject) {
+  // 如果相等了，说明return的是自己，抛出类型错误并返回
+  if (promise === x) {
+    return reject(
+      new TypeError('The promise and the return value are the same')
+    )
+  }
 
-n1.then((data) => {
-  console.log('111', data)
-  return 5
-  // return new SelfPromise((resolve, reject) => {
-  //   resolve('成功了2')
-  // })
-}).then((data) => {
-  console.log('222', data)
-})
+  if (typeof x === 'object' || typeof x === 'function') {
+    // x 为 null 直接返回，走后面的逻辑会报错
+    if (x === null) {
+      return resolve(x)
+    }
+
+    let then
+    try {
+      // 把 x.then 赋值给 then
+      then = x.then
+    } catch (error) {
+      // 如果取 x.then 的值时抛出错误 error ，则以 error 为据因拒绝 promise
+      return reject(error)
+    }
+
+    // 如果 then 是函数
+    if (typeof then === 'function') {
+      let called = false
+      try {
+        then.call(
+          x, // this 指向 x
+          // 如果 resolvePromise 以值 y 为参数被调用，则运行 [[Resolve]](promise, y)
+          (y) => {
+            // 如果 resolvePromise 和 rejectPromise 均被调用，
+            // 或者被同一参数调用了多次，则优先采用首次调用并忽略剩下的调用
+            // 实现这条需要前面加一个变量 called
+            if (called) return
+            called = true
+            resolvePromise(promise, y, resolve, reject)
+          },
+          // 如果 rejectPromise 以据因 r 为参数被调用，则以据因 r 拒绝 promise
+          (r) => {
+            if (called) return
+            called = true
+            reject(r)
+          }
+        )
+      } catch (error) {
+        // 如果调用 then 方法抛出了异常 error：
+        // 如果 resolvePromise 或 rejectPromise 已经被调用，直接返回
+        if (called) return
+
+        // 否则以 error 为据因拒绝 promise
+        reject(error)
+      }
+    } else {
+      // 如果 then 不是函数，以 x 为参数执行 promise
+      resolve(x)
+    }
+  } else {
+    // 如果 x 不为对象或者函数，以 x 为参数执行 promise
+    resolve(x)
+  }
+}
+
+MyPromise.deferred = function () {
+  var result = {}
+  result.promise = new MyPromise(function (resolve, reject) {
+    result.resolve = resolve
+    result.reject = reject
+  })
+
+  return result
+}
+
+module.exports = MyPromise
